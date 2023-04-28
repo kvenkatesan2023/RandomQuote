6e22c51ffb707f89426681487cf0634b863e21ab
kvenkatesan2023 <131791652+kvenkatesan2023@users.noreply.github.com>
2023-04-28 16:09:10 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,45 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-web';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+const kural = require('./kural.json');
 
-export default function App() {
+export default function Hai(){
+  const [randomField, setRandomField] = useState('');
+
+  useEffect(() => {
+    getRandomField();
+  }, []);
+
+  const getRandomField = () => {
+    const randomIndex = Math.floor(Math.random() * kural.length);
+    const randomItem = kural[randomIndex];
+    setRandomField(randomItem.Line1); // replace 'fieldName' with the actual field name you want to display
+  };
+
   return (
     <View style={styles.container}>
-      <View style={{width:'30%',justifyContent:'center',backgroundColor:'#f8f8ff',alignItems:'center',padding:20,borderRadius:20}}>
-         <Text style={{fontFamily:'arial',fontWeight:'bold',fontSize:20}}>Quote of the day </Text>
-         <Text style={{fontFamily:'arial',justifyContent:'center',fontWeight:'200',lineHeight:26,letterSpacing:1.2,margin:25}}> The smallest flower is a thought a life answering to some feature of the great whole of whom they have a president intution </Text>
-        
-          <TouchableOpacity
-             onPress={()=>{}}
-             style ={{
-               backgroundColor:'#4B66F0',
-               borderRadius:20,
-               padding:20,
-               marginVertical:20,
-               width:'40%',
-             }}> <Text style={{fontSize:15,fontWeight:'bold',textAlign:'center'}}>New Quote</Text>
-          </TouchableOpacity>
-          </View>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{randomField}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor:'#4B66F0',
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-    
-
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  book:{
-    borderRadius:'20%',
-    width:'20%',
-
-  }
-
-  
- 
+  text: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
 });
